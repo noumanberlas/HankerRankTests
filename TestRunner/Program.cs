7ae05b427c0af86013ce7a9b8e955438fc555d15
M Nouman Berlas <noumanberlas@gmail.com>
2023-05-05 15:02:57 -0400

    first commit
    first commit

---
+++ TestRunner/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//RunPlusMinusChallenge();
+
+RunMiniMaxSum();
+
+void RunPlusMinusChallenge()
+{
+    int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+    List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+    Result.plusMinus(arr);
+
+    Console.ReadLine();
+}
+
+void RunMiniMaxSum()
+{
+    List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+    Result.miniMaxSum(arr);
+}
---
+++ TestRunner/Result.cs
@@ -0,0 +1,162 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    /*
+ * Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
+
+Example
+
+There are  elements, two positive, two negative and one zero. Their ratios are ,  and . Results are printed as:
+
+0.400000
+0.400000
+0.200000
+Function Description
+
+Complete the plusMinus function in the editor below.
+
+plusMinus has the following parameter(s):
+
+int arr[n]: an array of integers
+Print
+Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with  digits after the decimal. The function should not return a value.
+
+Input Format
+
+The first line contains an integer, , the size of the array.
+The second line contains  space-separated integers that describe .
+
+Constraints
+
+
+
+Output Format
+
+Print the following  lines, each to  decimals:
+
+proportion of positive values
+proportion of negative values
+proportion of zeros
+Sample Input
+
+STDIN           Function
+-----           --------
+6               arr[] size n = 6
+-4 3 -9 0 4 1   arr = [-4, 3, -9, 0, 4, 1]
+Sample Output
+
+0.500000
+0.333333
+0.166667
+Explanation
+
+There are  positive numbers,  negative numbers, and  zero in the array.
+The proportions of occurrence are positive: , negative:  and zeros: .
+ * */
+
+    public static void plusMinus(List<int> arr)
+    {
+        double n = arr.Count;
+
+        double positiveCount = 0;
+        double negativeCount = 0;
+        double zeroCount = 0;
+
+        foreach (var item in arr)
+        {
+            if (item > 0)
+                positiveCount++;
+            else if (item < 0)
+                negativeCount++;
+            else zeroCount++;
+
+        }
+
+        string format = String.Format("{{0:0.{0}}}", new string('0', 6));
+
+        Console.WriteLine(String.Format(format, positiveCount / n));
+        Console.WriteLine(String.Format(format, negativeCount / n));
+        Console.WriteLine(String.Format(format, zeroCount / n));
+    }
+
+
+    /*
+     * Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+Example
+
+The minimum sum is  and the maximum sum is . The function prints
+
+16 24
+Function Description
+
+Complete the miniMaxSum function in the editor below.
+
+miniMaxSum has the following parameter(s):
+
+arr: an array of  integers
+Print
+
+Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+
+Input Format
+
+A single line of five space-separated integers.
+
+Constraints
+
+
+Output Format
+
+Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+Sample Input
+
+1 2 3 4 5
+Sample Output
+
+10 14
+Explanation
+
+The numbers are , , , , and . Calculate the following sums using four of the five integers:
+
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Hints: Beware of integer overflow! Use 64-bit Integer.
+      */
+
+    public static void miniMaxSum(List<int> t)
+    {
+        List<long> arr = t.ConvertAll(i => (long)i);
+        arr.Sort();
+        Console.Write(arr.Take(4).Sum() + " " + arr.TakeLast(4).Sum());
+    }
+
+}
+
+
